fbf7ae00c440aab20e73227a97e9f6abdd1ad0ba
Michael Wong <WongM@fnb-corp.com>
2022-12-28 15:20:31 -0500

    add esc keypress event

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ for (let i = 0; i < btnsOpenModal.length; ++i) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  if (
+    e.key === 'Escape' &&
+    !modal.classList.contains('hidden') &&
+    !overlay.classList.contains('hidden')
+  ) {
+    closeModal();
+  }
+});
+
 // Functions
 function closeModal() {
   modal.classList.add('hidden');
